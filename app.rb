ca6f2f705bfa9efc6447e8d44b2f2164957a3995
Yana Ninovska <125706832+yananin@users.noreply.github.com>
2024-03-22 06:15:26 +0000

    finished app

--- app.rb
+++ app.rb
@@ -1,9 +1,40 @@
 require "sinatra"
 require "sinatra/reloader"
 
+
+
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:square)
+end
+get("/hello") do
+  erb(:hello)
+end
+
+get("/square/new") do
+  erb(:square)
+end
+
+get("/square_root/new") do
+  erb(:root)
+end
+get("/payment/new") do
+  erb(:payment)
+end
+get("/random/new") do
+  erb(:random)
+end
+
+
+get("/square1") do
+  erb(:square1)
+end
+
+get("/root1") do
+  erb(:root1)
+end
+get("/payment1") do
+  erb(:payment1)
+end
+get("/random1") do
+  erb(:random1)
 end
